d50fa9410dbd69bff3406cd76b1f38eacb34ed99
SKolachana <83493845+SKolachana@users.noreply.github.com>
2022-12-02 22:46:12 +0530

    Delete main.py

--- src/main.py
+++
@@ -1,8 +0,0 @@
-import torch
-from torch_geometric.datasets import TUDataset
-from test_dataset import test_datasets
-
-dataset_name = ['MUTAG','NCI1', 'PROTEINS']
-for name in dataset_name:
-    dataset = TUDataset(root='.', name=name).shuffle()
-    test_datasets(dataset,name)
